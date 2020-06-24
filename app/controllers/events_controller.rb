67ebde2cfe0fe3f5dacbb7c7fa65b3392f938355
perunari <per.roc.ski@gmail.com>
2020-06-24 22:21:48 +0900

    event genre link

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,11 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    if params[:address_key]
+      @events = Event.where('address LIKE ?', "%#{params[:address_key]}%")
+    else
+      @events = Event.all
+    end
   end
 
   # GET /events/1
